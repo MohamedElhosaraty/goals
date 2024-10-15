b4879710c6024db00fc82cfd6c9e055cc3835245
MohamedElhosaraty <mohamedelhosaraty05@gmail.com>
2024-10-16 00:55:32 +0300

    create main_layout

--- lib/layout/main_layout/main_layout.dart
+++ lib/layout/main_layout/main_layout.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:goals/layout/cubit/main_cubit.dart';
+import 'package:goals/layout/main_layout/ui/floating_action_button.dart';
 import 'package:goals/layout/main_layout/ui/goal_item.dart';
 
 class MainLayout extends StatelessWidget {
@@ -36,89 +37,91 @@ class MainLayout extends StatelessWidget {
                     goalName: MainCubit.get(context).goalsList[index]['name'],
                   );
                 }),
-            floatingActionButton: FloatingActionButton(
-              backgroundColor: Colors.blue,
-              onPressed: () {
-                showDialog(
-                    context: context,
-                    builder: (_) => AlertDialog(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20)),
-                          content: SizedBox(
-                            height: 180.h,
-                            child: Column(
-                              children: [
-                                Container(
-                                  color: Colors.blue[900],
-                                  height: 30.h,
-                                  child: const Center(
-                                    child: Text(
-                                      "Write your Goals",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w700,
-                                          color: Colors.white),
-                                    ),
-                                  ),
-                                ),
-                                5.verticalSpace,
-                                TextFormField(
-                                  controller: MainCubit.get(context).addTextController,
-                                  decoration: InputDecoration(
-                                      filled: true,
-                                      fillColor: Colors.white,
-                                      label: const Text("Write Here....."),
-                                      floatingLabelBehavior:
-                                          FloatingLabelBehavior.never,
-                                      border: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(5)),
-                                      contentPadding: const EdgeInsets.all(5)),
-                                ),
-                                Row(
-                                  children: [
-                                    Expanded(
-                                      child: ElevatedButton(
-                                          style: ButtonStyle(
-                                              backgroundColor:
-                                                  WidgetStateProperty.all(
-                                                      Colors.red)),
-                                          onPressed: () {
-                                            MainCubit.get(context).insetData(
-                                                MainCubit.get(context).addTextController.text.toString());
 
-                                            MainCubit.get(context).addTextController.text = '';
-
-                                            MainCubit.get(context).getData();
-                                            Navigator.pop(context);
-                                          },
-                                          child: const Text("ADD")),
-                                    ),
-                                    5.horizontalSpace,
-                                    Expanded(
-                                      child: ElevatedButton(
-                                          style: ButtonStyle(
-                                              backgroundColor:
-                                                  WidgetStateProperty.all(
-                                                      Colors.blue)),
-                                          onPressed: () {
-                                            MainCubit.get(context).addTextController.text = '';
-                                            Navigator.pop(context);
-                                          },
-                                          child: const Text("Cancel")),
-                                    ),
-                                  ],
-                                )
-                              ],
-                            ),
-                          ),
-                        ));
-              },
-              child: Icon(
-                Icons.add,
-                color: Colors.white,
-                size: 33.sp,
-              ),
-            ),
+              floatingActionButton: const CreateFloatingActionButton(),
+            // floatingActionButton: FloatingActionButton(
+            //   backgroundColor: Colors.blue,
+            //   onPressed: () {
+            //     showDialog(
+            //         context: context,
+            //         builder: (_) => AlertDialog(
+            //               shape: RoundedRectangleBorder(
+            //                   borderRadius: BorderRadius.circular(20)),
+            //               content: SizedBox(
+            //                 height: 180.h,
+            //                 child: Column(
+            //                   children: [
+            //                     Container(
+            //                       color: Colors.blue[900],
+            //                       height: 30.h,
+            //                       child: const Center(
+            //                         child: Text(
+            //                           "Write your Goals",
+            //                           style: TextStyle(
+            //                               fontWeight: FontWeight.w700,
+            //                               color: Colors.white),
+            //                         ),
+            //                       ),
+            //                     ),
+            //                     5.verticalSpace,
+            //                     TextFormField(
+            //                       controller: MainCubit.get(context).addTextController,
+            //                       decoration: InputDecoration(
+            //                           filled: true,
+            //                           fillColor: Colors.white,
+            //                           label: const Text("Write Here....."),
+            //                           floatingLabelBehavior:
+            //                               FloatingLabelBehavior.never,
+            //                           border: OutlineInputBorder(
+            //                               borderRadius:
+            //                                   BorderRadius.circular(5)),
+            //                           contentPadding: const EdgeInsets.all(5)),
+            //                     ),
+            //                     Row(
+            //                       children: [
+            //                         Expanded(
+            //                           child: ElevatedButton(
+            //                               style: ButtonStyle(
+            //                                   backgroundColor:
+            //                                       WidgetStateProperty.all(
+            //                                           Colors.red)),
+            //                               onPressed: () {
+            //                                 MainCubit.get(context).insetData(
+            //                                     MainCubit.get(context).addTextController.text.toString());
+            //
+            //                                 MainCubit.get(context).addTextController.text = '';
+            //
+            //                                 MainCubit.get(context).getData();
+            //                                 Navigator.pop(context);
+            //                               },
+            //                               child: const Text("ADD")),
+            //                         ),
+            //                         5.horizontalSpace,
+            //                         Expanded(
+            //                           child: ElevatedButton(
+            //                               style: ButtonStyle(
+            //                                   backgroundColor:
+            //                                       WidgetStateProperty.all(
+            //                                           Colors.blue)),
+            //                               onPressed: () {
+            //                                 MainCubit.get(context).addTextController.text = '';
+            //                                 Navigator.pop(context);
+            //                               },
+            //                               child: const Text("Cancel")),
+            //                         ),
+            //                       ],
+            //                     )
+            //                   ],
+            //                 ),
+            //               ),
+            //             ));
+            //   },
+            //   child: Icon(
+            //     Icons.add,
+            //     color: Colors.white,
+            //     size: 33.sp,
+            //   ),
+            // ),
           );
         },
       ),
