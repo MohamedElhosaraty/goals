9dcd33270d28dc6ae12e19add44d2f848bca50d7
MohamedElhosaraty <mohamedelhosaraty05@gmail.com>
2024-10-17 03:06:59 +0300

    create add language to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:goals/layout/cubit/main_cubit.dart';
 import 'package:goals/layout/main_layout/main_layout.dart';
+import 'package:goals/localization/set_localization.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,18 +20,36 @@ class MyApp extends StatelessWidget {
         minTextAdapt: true,
         splitScreenMode: true,
         builder: (_, child) {
-         return MaterialApp(
-           debugShowCheckedModeBanner: false,
-            title: 'Goals',
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
+          return BlocProvider(
+            create: (context) => MainCubit()
+              ..initSqfLite()
+              ..getData(),
+            child: BlocBuilder<MainCubit, MainState>(
+              builder: (context, state) {
+                return MaterialApp(
+                  locale: Locale(MainCubit.get(context).lang.toString()),
+                  supportedLocales: const [
+                    Locale('en', 'US'),
+                    Locale('ar', 'EG')
+                  ],
+                  localizationsDelegates: const [
+                    SetLocalization.localizationsDelegate,
+                    GlobalMaterialLocalizations.delegate,
+                    GlobalWidgetsLocalizations.delegate,
+                    GlobalCupertinoLocalizations.delegate,
+                  ],
+                  debugShowCheckedModeBanner: false,
+                  title: 'Goals',
+                  theme: ThemeData(
+                    colorScheme:
+                        ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                    useMaterial3: true,
+                  ),
+                  home: const MainLayout(),
+                );
+              },
             ),
-            home: const MainLayout(),
           );
-        }
-    );
+        });
   }
 }
-
-
